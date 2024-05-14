2e916c21411bbe48581e7cbbd4b9a525ce9a9ac5
이선호 <skiplsh03@gmail.com>
2024-05-14 19:40:21 +0900

    Update NewBehaviourScript.cs

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Vector3 Position;
     void Start()
     {
         
